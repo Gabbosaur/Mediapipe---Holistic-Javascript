fe2d14aa8cdb3dfcdda216903d84f265435e8f05
lucasamp94 <62544838+lucasamp94@users.noreply.github.com>
2021-06-21 17:07:53 +0200

    aaaaa

--- Mediapipe-Python/main.py
+++ Mediapipe-Python/main.py
@@ -29,43 +29,50 @@ cv2.destroyAllWindows()
 
 
 def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
+	a = np.array(a)  # First
+	b = np.array(b)  # Mid
+	c = np.array(c)  # End
 
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
-        a[1] - b[1], a[0] - b[0]
-    )
-    angle = np.abs(radians * 180.0 / np.pi)
+	radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
+		a[1] - b[1], a[0] - b[0]
+	)
+	angle = np.abs(radians * 180.0 / np.pi)
 
-    if angle > 180.0:
-        angle = 360 - angle
+	if angle > 180.0:
+		angle = 360 - angle
 
-    return angle
+	return angle
 
+rep_counter=0
+flag=0
 
 cap = cv2.VideoCapture(0)
 ## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
+	while cap.isOpened():
+		ret, frame = cap.read()
 
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+		# Recolor image to RGB
+		image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		image.flags.writeable = False
 
-        # Make detection
-        results = pose.process(image)
+		# Make detection
+		results = pose.process(image)
 
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+		# Recolor back to BGR
+		image.flags.writeable = True
+		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Extract landmarks
-        try:
-            landmarks = results.pose_landmarks.landmark
-            # print(landmarks)
-            """
+		# Extract landmarks
+		try:
+			landmarks = results.pose_landmarks.landmark
+
+			cv2.putText(image,"BODY FOUND",
+						(15,15),
+						cv2.FONT_HERSHEY_SIMPLEX,0.5,(255, 255, 255),2,cv2.LINE_AA
+			)
+			# print(landmarks)
+			"""
 			ciclo ogni landmarks
 			for lndmrk in mp_pose.PoseLandmark:
 				print(lndmrk)
@@ -73,112 +80,104 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 			accesso ai lendmark
 			landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x oppure .y  .z  .visibility
 			"""
-            # get coordinates
-            shoulder_left = [
-                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y,
-            ]
-            elbow_left = [
-                landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y,
-            ]
-            wrist_left = [
-                landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y,
-            ]
-            hip_left = [
-                landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
-                landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y,
-            ]
+			# get coordinates
+			shoulder_left = [
+				landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+				landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y,
+			]
+			elbow_left = [
+				landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+				landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y,
+			]
+			wrist_left = [
+				landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+				landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y,
+			]
+			hip_left = [
+				landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+				landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y,
+			]
 
-            shoulder_right = [
-                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y,
-            ]
-            elbow_right = [
-                landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
-                landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y,
-            ]
-            wrist_right = [
-                landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
-                landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y,
-            ]
-            hip_right = [
-                landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
-                landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y,
-            ]
+			shoulder_right = [
+				landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+				landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y,
+			]
+			elbow_right = [
+				landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+				landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y,
+			]
+			wrist_right = [
+				landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+				landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y,
+			]
+			hip_right = [
+				landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+				landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y,
+			]
 
-            # calculate angles
-            angle_elbow_left = calculate_angle(shoulder_left, elbow_left, wrist_left)
-            angle_elbow_right = calculate_angle(
-                shoulder_right, elbow_right, wrist_right
-            )
-            angle_shoulder_left = calculate_angle(hip_left, shoulder_left, elbow_left)
-            angle_shoulder_right = calculate_angle(
-                hip_right, shoulder_right, elbow_right
-            )
+			# calculate angles
+			angle_elbow_left = calculate_angle(shoulder_left, elbow_left, wrist_left)
+			angle_elbow_right = calculate_angle(
+				shoulder_right, elbow_right, wrist_right
+			)
+			angle_shoulder_left = calculate_angle(hip_left, shoulder_left, elbow_left)
+			angle_shoulder_right = calculate_angle(hip_right, shoulder_right, elbow_right)
 
-            # Visualize angle
-            cv2.putText(
-                image,
-                str(math.trunc(angle_elbow_left)) + "deg",
-                tuple(np.multiply(elbow_left, [640, 480]).astype(int)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
-            )
+			# Visualize angle
+			cv2.putText(image,str(math.trunc(angle_elbow_left)) + "deg",
+				tuple(np.multiply(elbow_left, [640, 480]).astype(int)),
+				cv2.FONT_HERSHEY_SIMPLEX,0.5,(255, 255, 255),2,cv2.LINE_AA
+			)
 
-            cv2.putText(
-                image,
-                str(math.trunc(angle_elbow_right)) + "deg",
-                tuple(np.multiply(elbow_right, [640, 480]).astype(int)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
-            )
+			cv2.putText(image,str(math.trunc(angle_elbow_right)) + "deg",
+				tuple(np.multiply(elbow_right, [640, 480]).astype(int)),
+				cv2.FONT_HERSHEY_SIMPLEX,0.5,(255, 255, 255),2,cv2.LINE_AA
+			)
 
-            cv2.putText(
-                image,
-                str(math.trunc(angle_shoulder_left)) + "deg",
-                tuple(np.multiply(shoulder_left, [640, 480]).astype(int)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
-            )
+			cv2.putText(image,str(math.trunc(angle_shoulder_left)) + "deg",
+						tuple(np.multiply(shoulder_left, [640, 480]).astype(int)),
+						cv2.FONT_HERSHEY_SIMPLEX,0.5,(255, 255, 255),2,cv2.LINE_AA
+			)
 
-            cv2.putText(
-                image,
-                str(math.trunc(angle_shoulder_right)) + "deg",
-                tuple(np.multiply(shoulder_right, [640, 480]).astype(int)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
-            )
+			cv2.putText(image,str(math.trunc(angle_shoulder_right)) + "deg",
+				tuple(np.multiply(shoulder_right, [640, 480]).astype(int)),
+				cv2.FONT_HERSHEY_SIMPLEX,0.5,(255, 255, 255),2,cv2.LINE_AA
+			)
+			tollerance = 20
+			if (angle_elbow_left >= (180 - tollerance) & angle_elbow_left <= (180 + tollerance) & angle_elbow_right >= (180 - tollerance) & angle_elbow_right <= (180 + tollerance) & angle_shoulder_left <= (90 + tollerance) & angle_shoulder_left >= (90 - tollerance) & angle_shoulder_right <= (90 + tollerance) & angle_shoulder_right >= (90 - tollerance)):
+				cv2.putText(image,"T-Pose found",
+						(60,60),
+						cv2.FONT_HERSHEY_SIMPLEX,0.5,(255, 255, 255),2,cv2.LINE_AA
+				)
+				if (flag == 0):
+					rep_counter = rep_counter+1
+					flag = 1
+				else:
+					if (angle_shoulder_left < 20 & angle_shoulder_right < 20):
+						flag = 0
+		except:
+			pass
+		
+		#status box
+		cv2.rectangle(image, (0,0), (225,80), (245,117,16), -1)
 
-        except:
-            pass
+		# Rep data
+		cv2.putText(image, 'REPS', (15,15), 
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+		cv2.putText(image, str(rep_counter), 
+					(10,63), 
+					cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+		
+		# Render detections
+		mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,
+			mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2),
+			mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2),
+		)  # color=(b,g,r)
 
-        # Render detections
-        mp_drawing.draw_landmarks(
-            image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2),
-            mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2),
-        )  # color=(b,g,r)
+		cv2.imshow("Mediapipe Feed", image)
 
-        cv2.imshow("Mediapipe Feed", image)
+		if cv2.waitKey(10) & 0xFF == ord("q"):
+			break
 
-        if cv2.waitKey(10) & 0xFF == ord("q"):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+	cap.release()
+	cv2.destroyAllWindows()
