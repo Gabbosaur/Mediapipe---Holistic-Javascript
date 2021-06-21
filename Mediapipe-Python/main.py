7a3426fa0a3bdf6edcc4a0dc79ddfb9f5df4d839
lucasamp94 <62544838+lucasamp94@users.noreply.github.com>
2021-06-21 16:14:36 +0200

    prova simbolo grado

--- Mediapipe-Python/main.py
+++ Mediapipe-Python/main.py
@@ -6,9 +6,11 @@
 
 # pip install mediapipe opencv-python
 
+
 import cv2
 import mediapipe as mp
 import numpy as np
+import math
 
 mp_drawing=mp.solutions.drawing_utils	#drawing utilities
 mp_pose=mp.solutions.pose				#pose estimation model di mediapipe
@@ -24,6 +26,19 @@ while cap.isOpened():
 cap.release()
 cv2.destroyAllWindows()
 '''
+
+def calculate_angle(a,b,c):
+	a = np.array(a) # First
+	b = np.array(b) # Mid
+	c = np.array(c) # End
+
+	radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+	angle = np.abs(radians*180.0/np.pi)
+	'''
+	if angle >180.0:
+		angle = 360-angle
+	'''
+	return angle
 
 cap = cv2.VideoCapture(0)
 ## Setup mediapipe instance
@@ -42,13 +57,74 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 		image.flags.writeable = True
 		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+		# Extract landmarks
+		try:
+			landmarks = results.pose_landmarks.landmark
+			#print(landmarks)
+			'''
+			ciclo ogni landmarks
+			for lndmrk in mp_pose.PoseLandmark:
+				print(lndmrk)
+
+			accesso ai lendmark
+			landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x oppure .y  .z  .visibility
+			'''
+			#get coordinates
+			shoulder_left = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+			elbow_left = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+			wrist_left = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+			hip_left = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+
+			shoulder_right = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+			elbow_right = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+			wrist_right = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+			hip_right = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+			
+			#calculate angles
+			angle_elbow_left=calculate_angle(shoulder_left, elbow_left, wrist_left)
+			angle_elbow_right=calculate_angle(shoulder_right, elbow_right, wrist_right)
+			angle_shoulder_left=calculate_angle(hip_left,shoulder_left, elbow_left)
+			angle_shoulder_right=calculate_angle(hip_right,shoulder_right, elbow_right)
+
+			degree_sign= u"\N{DEGREE SIGN}"
+			print(degree_sign)
+			# Visualize angle
+			cv2.putText(image, str(math.trunc(angle_elbow_left)) + '°', 
+						tuple(np.multiply(elbow_left, [640, 480]).astype(int)), 
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+						)
+
+			cv2.putText(image, str(math.trunc(angle_elbow_right)) + '°', 
+						tuple(np.multiply(elbow_right, [640, 480]).astype(int)), 
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+						)
+
+			cv2.putText(image, str(math.trunc(angle_shoulder_left)) + degree_sign, 
+						tuple(np.multiply(shoulder_left, [640, 480]).astype(int)), 
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+						)
+
+			cv2.putText(image, str(math.trunc(angle_shoulder_right)) + '°', 
+						tuple(np.multiply(shoulder_right, [640, 480]).astype(int)), 
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+						)
+
+		except:
+			pass
+
 		# Render detections
-		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2),mp_drawing.DrawingSpec(color=(0,255,0), thickness=2))	#color=(b,g,r)
+		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+									mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2),
+									mp_drawing.DrawingSpec(color=(0,255,0), thickness=2))	#color=(b,g,r)
 
 		cv2.imshow('Mediapipe Feed', image)
 
 		if cv2.waitKey(10) & 0xFF == ord('q'):
 			break
+
+		
+
+
 
 	cap.release()
 	cv2.destroyAllWindows()
